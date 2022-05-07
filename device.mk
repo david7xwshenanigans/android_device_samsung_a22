cf19294c0a871c3a050b0ed10d672d80ad938d68
Vaisakh Murali <mvaisakh@statixos.com>
2022-05-07 12:23:14 +0100

    a22: Move power and perf configs to device tree
    This would help changing perf and power HAL parameters in userspace.
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- device.mk
+++ device.mk
@@ -260,7 +260,12 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     power.default
 
-# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/power_app_cfg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_app_cfg.xml \
+    $(LOCAL_PATH)/configs/perf/powercontable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powercontable.xml \
+    $(LOCAL_PATH)/configs/perf/powerscntbl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerscntbl.xml
+
+# Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
