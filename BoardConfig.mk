3c02bf7d92a945637cb6d71eba171a004f1de5da
bengris32 <bengris32@protonmail.ch>
2022-12-10 10:19:23 +0000

    a22: Define Bootloader and platform flags
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Ia19a04d66d39ce6af38fb33afadc361743ecd56c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,22 +1,32 @@
-soong_namespace {
-    imports: [
-        "hardware/google/interfaces",
-        "hardware/google/pixel",
-    ],
-}
 #
-# Copyright (C) 2020-2025 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: Apache-2.0
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 DEVICE_PATH := device/samsung/a22
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := a22
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6765
+
+# Inherit the proprietary files
+include vendor/samsung/a22/BoardConfigVendor.mk
