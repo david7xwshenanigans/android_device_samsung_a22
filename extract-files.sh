4a6714939c6a7a469d35af566dc5930eaa9e2329
Himanshu Tyagi <himanshut567@gmail.com>
2022-12-21 11:27:23 +0530

    a22: Rename libcode2_vndk to avoid conflict
    AOSP one gives symbol error - F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.media.c2@1.2-mediatek": cannot locate symbol "_ZTVN7android5FenceE" referenced by "/vendor/lib/libcodec2_vndk.so"...
    
    lets use prebuilt and rename it.
    
    Signed-off-by: Himanshu Tyagi <himanshut567@gmail.com>
    Change-Id: Ic6da41abe0491c6be4548984c98c04a35e61a25c

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,11 @@ fi
 
 function blob_fixup {
     case "$1" in
+        vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
+            ;&
+        vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
+           "${PATCHELF}" --replace-needed "libcodec2_vndk.so" "libcodec2_vndk-mtk.so" "${2}"
+            ;;
         vendor/bin/hw/android.hardware.sensors@2.0-service.multihal)
             "$PATCHELF" --replace-needed libutils.so libutils-v32.so "$2"
             ;;
