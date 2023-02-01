7f72b18c6feada5f52dc22e17c48b3059474e7da
bengris32 <bengris32@protonmail.ch>
2023-02-01 20:47:33 +0000

    a22: Source build mtkpower interfaces
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I39fab0ac23522b872f2455d2cc6d78e95e87b2fe

--- device.mk
+++ device.mk
@@ -259,7 +259,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-mediatek \
     android.hardware.power@1.2.vendor \
-    power.default
+    power.default \
+    vendor.mediatek.hardware.mtkpower@1.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/power_app_cfg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_app_cfg.xml \
