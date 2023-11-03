cda415976224fe32e289e159f0aef399433e2125
bengris32 <bengris32@protonmail.ch>
2023-11-03 10:19:17 +0100

    a22: Switch to AOSP audio service
    Change-Id: I933b66e3821562022eb837af91ad4039e22fb578
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio.service.mediatek \
+    android.hardware.audio.service \
     libaudioclient_aidl_conversion \
     libaudiofoundation.vendor \
     libbluetooth_audio_session \
