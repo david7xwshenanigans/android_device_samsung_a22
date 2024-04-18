3653a519b5935011f2a7664cd81b458797a5d25e
AndroidHQ254 <d.murimi14@gmail.com>
2024-04-18 23:55:25 +0300

    a22: Build Wi-Fi interfaces from source

--- device.mk
+++ device.mk
@@ -74,5 +74,13 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    android.hardware.tetheroffload.config@1.0.vendor \
+    android.hardware.tetheroffload.control@1.1.vendor \
+    android.hardware.wifi.hostapd@1.3.vendor \
+    android.hardware.wifi.supplicant@1.4.vendor \
+    android.hardware.wifi@1.5.vendor
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/samsung/a22/a22-vendor.mk)
