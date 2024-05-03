54b12b965f91afb56beecc7e7518abe3e4185405
Sarthak Roy <sarthakroy2002@gmail.com>
2024-05-03 19:37:47 +0300

    a22: Properly fix offline charging

--- device.mk
+++ device.mk
@@ -86,6 +86,12 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images_vendor \
+    product_charger_res_images_vendor \
+    libsuspend
+
 # Disable priv-app property whitelist
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.control_privapp_permissions=log
