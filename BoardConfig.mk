f2a90d5c033fa0af91409e9c2d13a20c7ccfc895
david7xw <davdavid7xw@gmail.com>
2025-11-28 21:16:57 +0200

    a22: generate initial FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,7 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 
 # VNDK
 BOARD_VNDK_VERSION := current
