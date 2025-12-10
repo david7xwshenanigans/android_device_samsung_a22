978b50c135e6edcd41f85f3eb4a9a07dcaffdf3f
david7xw <davdavid7xw@gmail.com>
2025-12-10 12:41:14 +0200

    a22: label stock GNSS service and address MNLD denial

--- device.mk
+++ device.mk
@@ -142,7 +142,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
     android.hardware.gnss@2.1.vendor \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
-    android.hardware.gnss.visibility_control@1.0.vendor
+    android.hardware.gnss.visibility_control@1.0.vendor 
 
 # Health
 PRODUCT_PACKAGES += \
