f67402e204f8859538cfa9779c7efa160aefda63
Zears14 <79321017+Zears14@users.noreply.github.com>
2025-12-24 07:45:27 +0700

    a22: usb: fix coral implementation and also fix usb props

--- device.mk
+++ device.mk
@@ -380,6 +380,7 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor
 
 # USB
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/android.hardware.usb@1.3-service.coral.xml
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor
 
