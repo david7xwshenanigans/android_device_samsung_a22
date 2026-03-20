5d5efa14bb8a408ebe04259fa27f667f6774cfc4
Zears14 <79321017+Zears14@users.noreply.github.com>
2026-03-21 01:03:08 +0700

    a22: Add support for PixelOS and whatever rom uses custom_a22.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a22.mk
+    $(LOCAL_DIR)/lineage_a22.mk \
+    $(LOCAL_DIR)/custom_a22.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_a22-user \
     lineage_a22-userdebug \
-    lineage_a22-eng
+    lineage_a22-eng \
+    custom_a22-user \
+    custom_a22-userdebug \
+    custom_a22-eng
--- lineage_a22.mk
+++ lineage_a22.mk
@@ -4,23 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from a22 device
-$(call inherit-product, device/samsung/a22/device.mk)
+# Inherit common a22 device config
+$(call inherit-product, device/samsung/a22/common_a22.mk)
 
-PRODUCT_DEVICE := a22
 PRODUCT_NAME := lineage_a22
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-A225F
-PRODUCT_MANUFACTURER := samsung
-
-PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
-PRODUCT_BUILD_DESCRIPTION := a22nsxx-user 13 TP1A.220624.014 A225FXXSBDYE1 release-keys
-
-BUILD_FINGERPRINT := samsung/a22nsxx/a22:13/TP1A.220624.014/A225FXXSBDYE1:user/release-keys
