af0756210f089dd67d33c61c3661175127522f4e
david7xw <davdavid7xw@gmail.com>
2026-04-05 14:39:34 +0300

    a22: sepolicy: initial public sepolicy import from Galaxy A32
    Not the greatest method out there, but since the devices are similar, it should do the trick...
    Source: https://github.com/A325F/device_samsung_a32/tree/lineage-21-A325F/sepolicy/public
    Co-authored-by: AndroidHQ254 <d.murimi14@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,6 +156,7 @@ VENDOR_SECURITY_PATCH := 2025-05-01
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
